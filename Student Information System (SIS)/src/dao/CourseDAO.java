5482b7732142f0e300cb9374a127eca2cb0f68ac
ADTANU <104961756+ADTANU@users.noreply.github.com>
2025-04-17 15:52:03 +0530

    Update CourseDAO.java

--- Student Information System (SIS)/src/dao/CourseDAO.java
+++ Student Information System (SIS)/src/dao/CourseDAO.java
@@ -13,11 +13,11 @@ import exception.TeacherNotFoundException;
 public interface CourseDAO {
 	
 	Course getCourse(int courseId) throws CourseNotFoundException; 
-    void assignTeacher(int courseId, Teacher teacher) throws TeacherNotFoundException, InvalidTeacherDataException; // Assigns a teacher to the course
-    void updateCourseInfo(int courseId, String courseCode, String courseName, String instructorName) throws InvalidCourseDataException, CourseNotFoundException; // Updates course information
-    void displayCourseInfo(int courseId) throws CourseNotFoundException; // Displays detailed information about the course
-    List<Enrollment> getEnrollments(int courseId) throws CourseNotFoundException; // Retrieves a list of student enrollments for the course
-    Teacher getAssignedTeacher(int courseId) throws TeacherNotFoundException; // Retrieves the assigned teacher for the course
+    void assignTeacher(int courseId, Teacher teacher) throws TeacherNotFoundException, InvalidTeacherDataException;
+    void updateCourseInfo(int courseId, String courseCode, String courseName, String instructorName) throws InvalidCourseDataException, CourseNotFoundException; 
+    void displayCourseInfo(int courseId) throws CourseNotFoundException; 
+    List<Enrollment> getEnrollments(int courseId) throws CourseNotFoundException;
+    Teacher getAssignedTeacher(int courseId) throws TeacherNotFoundException;
 	void addCourse(Course course);
 	List<Course> getAllCourses();
 }
