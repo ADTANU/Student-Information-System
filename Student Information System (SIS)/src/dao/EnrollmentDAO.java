948dfc51867204bf7e14eadc3605d24c86cea916
ADTANU <104961756+ADTANU@users.noreply.github.com>
2025-04-17 15:53:19 +0530

    Update EnrollmentDAO.java

--- Student Information System (SIS)/src/dao/EnrollmentDAO.java
+++ Student Information System (SIS)/src/dao/EnrollmentDAO.java
@@ -9,7 +9,7 @@ import exception.InvalidEnrollmentDataException;
 import exception.StudentNotFoundException;
 
 public interface EnrollmentDAO {
-    Student getStudent(int enrollmentId) throws StudentNotFoundException; // Retrieves the student associated with the enrollment
-    Course getCourse(int enrollmentId) throws CourseNotFoundException; // Retrieves the course associated with the enrollment
-    void addEnrollment(Enrollment enrollment) throws InvalidEnrollmentDataException, DuplicateEnrollmentException; // Adds a new enrollment
+    Student getStudent(int enrollmentId) throws StudentNotFoundException;
+    Course getCourse(int enrollmentId) throws CourseNotFoundException; 
+    void addEnrollment(Enrollment enrollment) throws InvalidEnrollmentDataException, DuplicateEnrollmentException; 
 }
